2cef4629c77edcf54f2f01f4b8426406ae1d8db2
Andrey Maslov <109115043+Gorchitza7@users.noreply.github.com>
2024-05-14 01:51:08 +0300

    Add files via upload

---
+++ Task_2/Task_2.js
@@ -0,0 +1,14 @@
+// Task 2
+
+const obj = {
+	name : "Anton",
+	age : 36,
+	skills : ["Javascript", "HTML", "CSS"],
+	salary : 80000
+};
+
+const result = JSON.stringify(obj);
+console.log(result);
+
+// образец:
+// {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
